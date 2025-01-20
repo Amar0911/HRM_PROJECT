9c66812503ada467214acc0d012d71be5c7a67d5
Amar0911 <guptaamar0911@gmail.com>
2025-01-20 12:29:19 +0530

    Initial commit

---
+++ HRMS/department/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add/', views.add_department, name='add_department'),
+    path('update/<int:id>/', views.update_department, name='update_department'),
+    path('delete/<int:id>/', views.delete_department, name='delete_department'),
+    path('',views.login_view,name='login'),
+    path('logout/',views.logout_view,name='logout'),
+]
+
+
+
+
+
